59a5ffa6f5c004619fece8544a45a42df889ad53
salexir <17128987+salexir@users.noreply.github.com>
2025-01-18 18:27:44 +0400

    Reorganise app sections, and add a new budget making section

--- eclairCitoyen/server.R
+++ eclairCitoyen/server.R
@@ -1,11 +1,44 @@
 server <- function(input, output){
-
+  
   
   ## Category Spending Things --------------------------------------------------
-  ## Merchant Analysis Things --------------------------------------------------
-  ## Time Series Analysis Things -----------------------------------------------
   
-  ### Total Spending Over Time -------------------------------------------------
+  
+  
+  totalSpendingOverTime_rolledUp <-
+    reactive({
+      fin_data %>%
+        filter(category %in% input$in_totalSpendingOverTime_rolledUp,
+               category != "Non-Spend") %>%
+        group_by(yyyymm, category) %>%
+        summarise(Total_Spends = (sum(Signed_Split_Amount))*-1) %>%
+        ungroup() %>%
+        mutate(yyyymm = as.Date(paste0(yyyymm, "-01"))) # Allows for nicer date output in ggplot
+      
+    })
+  
+  
+  output$out_totalSpendingOverTime_rolledUp <-
+    renderPlot(
+    
+      totalSpendingOverTime_rolledUp() %>% 
+        ggplot(aes(x = yyyymm, y = Total_Spends, colour = category, group = category)) + 
+        geom_line() + 
+        geom_point() + 
+        geom_smooth(se = FALSE) + 
+        scale_x_date(date_labels = "%b %y", date_breaks = "1 month", 
+                     date_minor_breaks = "3 month", 
+                     limits = c(as.Date("2021-01-01"), NA)) + 
+        theme_lab() + scale_y_continuous(labels = scales::dollar) +   
+        ggtitle("Spending by Category, over time") + ylab("Nominal CAD ($)") + 
+        xlab("YYYY-MM") + 
+        theme(legend.title = element_blank())
+    
+    )
+  
+  output$out_totalSpendingOverTime_rolledUp_info <- renderDT({
+    brushedPoints(totalSpendingOverTime_rolledUp(), input$plot_brush)
+  })
   
   totalSpendingOverTime <- 
     reactive({
@@ -13,7 +46,7 @@ server <- function(input, output){
       fin_data %>%
         filter(model_outputs_validated %in% input$in_totalSpendingOverTime) %>%
         group_by(model_outputs_validated, Date) %>%
-        summarise(Total_Spends = sum(Split_Amount))
+        summarise(Total_Spends = (sum(Signed_Split_Amount))*-1)
       
       
     })
@@ -23,8 +56,13 @@ server <- function(input, output){
       totalSpendingOverTime() %>%
         ggplot(aes(x = Date, y = Total_Spends, colour = model_outputs_validated,
                    group = model_outputs_validated)) + 
-        geom_point() + geom_line() + scale_x_date(breaks = "6 months") + theme_lab() + scale_y_continuous(labels = scales::dollar) + 
-        ggtitle("Spending Over Time, by Category") + ylab("Nominal CAD ($)") + xlab("YYYY-MM") + theme(legend.title = element_blank())
+        geom_point() + geom_line() +  
+        scale_x_date(date_labels = "%b %y", date_breaks = "3 month", date_minor_breaks = "1 month", limits = c(as.Date("2021-01-01"), NA)) + theme_lab() + scale_y_continuous(labels = scales::dollar) + 
+        ggtitle("Spending by Category, over time") + ylab("Nominal CAD ($)") + xlab("YYYY-MM") + 
+        theme(legend.title = element_blank(),
+              panel.grid.minor.x = element_line(size = 0.15, color = "#f1f1f1", linetype = "longdash"),
+              panel.grid.major.x = element_line(size = 0.25, color = "#cbcbcb", linetype = "longdash")
+        )
       
     )
   
@@ -33,13 +71,136 @@ server <- function(input, output){
   })
   
   
+  output$out_totalSpendingOverTime_table <- renderDT({
+    
+    fin_data %>%
+      filter(category != "Non-Spend",
+             yyyy == input$in_totalSpendingOverTime_table) %>%
+      mutate(num_months = n_distinct(yyyymm)) %>%
+      group_by(yyyy, yyyymm, num_months, model_outputs_validated) %>%
+      summarise(Total = round(sum(Signed_Split_Amount)*-1, digits = 2)) %>%
+      mutate(month_viz = format(as.Date(paste0(yyyymm, "-01")), "%b %Y")) %>%
+      group_by(yyyy, model_outputs_validated) %>%
+      mutate(YTD = round(sum(Total), 2),
+             Monthly_Average = round(YTD/num_months, 2)) %>%
+      ungroup() %>%
+      select(-c(yyyymm, num_months, yyyy)) %>%
+      pivot_wider(names_from = month_viz, values_from = Total) %>%
+      relocate(c(YTD, Monthly_Average), .after = last_col()) %>%
+      arrange(model_outputs_validated)
+    
+  })
+  
+  
+  comparativeSpends <- 
+    reactive({
+      
+      fin_data %>% 
+        filter(model_outputs_validated %in% input$in_comparativeSpends_monthly_yoy) %>%
+        mutate(month = factor(format(Date, "%b"), month.abb, ordered = TRUE)) %>% 
+        group_by(yyyy, month, model_outputs_validated) %>% 
+        summarise(Total_Spends = sum(Split_Amount))
+      
+    })
+  
+  
+  output$out_comparativeSpends_monthly_yoy <- renderPlot({
+    
+    comparativeSpends() %>% 
+      ggplot(aes(x = month, y = Total_Spends, fill = yyyy, group = yyyy)) + 
+      geom_col(position = "dodge") +
+      facet_wrap(~model_outputs_validated) + 
+      scale_fill_brewer(type = "seq") + 
+      theme_lab() + scale_y_continuous(labels = scales::dollar)
+    
+    
+    
+  })
+  
+  ## Merchant Analysis Things --------------------------------------------------
+  ## Time Series Analysis Things -----------------------------------------------
+  
+  ### Total Spending Over Time -------------------------------------------------
+  
+  data_input1 <- dataInputServer("input1")
+  
+  output$test_edit <- renderRHandsontable(rhandsontable(iris))
+  
+  
+  
   
   ## Spending Patterns Things --------------------------------------------------
+  
+  output$out_number_of_transactions <- renderDT({
+    
+    fin_data %>%
+      filter(category != "Non-Spend",
+             yyyy %in% input$in_number_of_transactions, 
+             Transaction_Type == "Debit") %>% # Refunds (via crediting) don't count as a distinct transaction, hence removed.
+      mutate(num_months = n_distinct(yyyymm)) %>%
+      group_by(yyyy, yyyymm, num_months, model_outputs_validated) %>%
+      summarise(Number_of_transactions =n() ) %>%
+      mutate(month_viz = format(as.Date(paste0(yyyymm, "-01")), "%b %Y")) %>%
+      group_by(yyyy, model_outputs_validated) %>%
+      mutate(YTD = round(sum(Number_of_transactions), 2),
+             Monthly_Average = round(YTD/num_months, 2)) %>%
+      ungroup() %>%
+      select(-c(yyyymm, num_months, yyyy)) %>%
+      pivot_wider(names_from = month_viz, values_from = Number_of_transactions) %>%
+      relocate(c(YTD, Monthly_Average), .after = last_col()) %>%
+      arrange(model_outputs_validated)
+    
+  })
+  
+  
+  ## Budget Making  ------------------------------------------------------------
+  
+  historical_averages <- 
+    reactive({
+      
+      fin_data %>% 
+        # Filtering out years without 12 month of data:
+        group_by(yyyy) %>% 
+        mutate(n_months = length(unique(yyyymm))) %>% 
+#        filter(n_months == 12) %>%
+        group_by(yyyy, model_outputs_validated) %>% 
+        summarise(`Num Transactions` = n(), 
+                  Total_Spends = sum(Signed_Split_Amount)*-1,
+                  Monthly_Average = round(Total_Spends/first(n_months), 2)) %>% 
+        select(-Total_Spends) %>%
+        pivot_wider(names_from = yyyy, values_from = c(`Num Transactions`, Monthly_Average))
+      
+      
+      
+    })
+  
+  output$out_historical_averages <- renderDT(historical_averages())
+  
+  ### Data Edit Section ----
+
+  #### Editor
+  budget_edit <- dataEditServer("budget_edit", data = budget_table, col_stretch = TRUE)
+  
+  
+
+  ### Data Output Section ----
+  final_budget <- reactive({budget_edit() %>% 
+                         mutate(Pred_yearly_spend = budgeted_amount * frequency,
+                                Pred_monthly_spend = Pred_yearly_spend/12) %>%
+                         relocate(Pred_yearly_spend, .after = last_col())})
+  
+  output$out_view_budget_final <- renderDT(final_budget())
+  
+  
+  
+  dataOutputServer(id = "save_budget", data = final_budget) # Important to not use the function form here.
+  
+  
   ## Full Data Table  ----------------------------------------------------------
   
   output$out_fullDataTable <- renderDT(fin_data %>% select(input$in_fullDataTable), filter = "top")
-                                       
-
+  
+  
   
   
   
--- eclairCitoyen/ui.R
+++ eclairCitoyen/ui.R
@@ -7,6 +7,8 @@ ui <- dashboardPage(
     
     sidebarMenu(
       
+      # SideBar Options ----
+      
       menuItem(" | Category Spending", tabName = "categorySpending", icon = icon("chart-column")),
       
       menuItem(" | Merchant Analysis", tabName = "merchantAnalysis", icon = icon("store")),
@@ -15,6 +17,9 @@ ui <- dashboardPage(
       
       
       menuItem(" | Spending Patterns", tabName = "spendingPatterns", icon = icon("basket-shopping")),
+      
+      
+      menuItem(" | Budget Making", tabName = "budgetMaking", icon = icon("feather")),
       
       
       menuItem(" | Full Data Table", tabName = "fullDataTable", icon = icon("table-cells"))
@@ -28,17 +33,41 @@ ui <- dashboardPage(
   ),
   
   dashboardBody(
-    
+      
 
     
     
     tabItems(
-      tabItem(tabName = "tsAnalysis",
+      # Tab Item: Category Spending ----
+      tabItem(tabName = "categorySpending", 
               fluidRow(
-      
-                
-                
-                tabBox(width = 9, 
+                tabBox(width = 12,
+                    title = "Overall Spending Trends",
+                    id = "totalSpendingOverTime_rolledUp_box",
+                    tabPanel("Graph",
+                             plotOutput(outputId = "out_totalSpendingOverTime_rolledUp", brush = "plot_brush"),
+                             dropdown(
+                               tags$h3("Controls"),
+                               
+                               virtualSelectInput(
+                                 inputId = "in_totalSpendingOverTime_rolledUp",
+                                 label = "Select groups: ",
+                                 choices = category_inputs,
+                                 selected = category_inputs,
+                                 multiple = TRUE,
+                                 search = TRUE
+                               ),
+                               circle = TRUE, status = "danger",
+                               icon = icon("gear"),size = "xs",
+                               
+                               tooltip = tooltipOptions(title = "Click to see inputs !")
+                               
+                             ),
+                             
+                             ),
+                    tabPanel("More Info", DTOutput("out_totalSpendingOverTime_rolledUp_info")))
+                    
+                ,tabBox(width = 12, 
                        title = "",
                        id = "spendingOverTime_tabbox",
                        tabPanel("Graph", 
@@ -47,7 +76,7 @@ ui <- dashboardPage(
                                   
                                   tags$h3("Controls"),
                                   
-                                 
+                                  
                                   virtualSelectInput(
                                     inputId = "in_totalSpendingOverTime",
                                     label = "Select groups :",
@@ -70,13 +99,156 @@ ui <- dashboardPage(
                                   
                                   tooltip = tooltipOptions(title = "Click to see inputs !")
                                 ),
-                                ),
+                       ),
                        tabPanel("More Info", DTOutput("out_totalSpendingOverTime_info"))
-                         )
+                ),
+                box(width = 12,
+                    title = "Category Spends by Month",
+                    id = "spendingOverTime_monthly",
+                    
+                    dropdownButton(
+                      
+                      tags$h3("Select year:"),
+                      
+                      awesomeRadio(
+                        inputId = "in_totalSpendingOverTime_table",
+                        label = "Radio buttons", 
+                        choices = sort(unique(fin_data$yyyy)),
+                        selected = rev(sort(unique(fin_data$yyyy)))[1]
+                      )
+                      
+                      
+                      
+                      
+                      ,
+                      
+                      circle = TRUE, status = "danger",
+                      icon = icon("gear"),size = "xs",
+                      
+                      tooltip = tooltipOptions(title = "Click to see inputs !")
+                    ),
+                    tabPanel("Table", DTOutput("out_totalSpendingOverTime_table")
+                    )
+                ),
+                
+                box(width = 12, 
+                    title = "Comparative Spends, by Month, YoY",
+                    id = "comparativeSpends_monthly_yoy",
+                    
+                    tabPanel("Test", plotOutput(outputId = "out_comparativeSpends_monthly_yoy"),
+                             dropdown(
+                               
+                               tags$h3("Controls"),
+                               
+                               
+                               virtualSelectInput(
+                                 inputId = "in_comparativeSpends_monthly_yoy",
+                                 label = "Select groups :",
+                                 choices = list(`Needs` = list(ui_inputs$Needs),
+                                                `Discretionary` = list(ui_inputs$Discretionary),
+                                                `Investment` = list(ui_inputs$Investment),
+                                                `Loan` = list(ui_inputs$Loan),
+                                                `Misc.` = list(ui_inputs$Misc.)),
+                                 selected = ui_inputs$Needs,
+                                 multiple = TRUE,
+                                 search = TRUE,
+                                 optionsCount = 5
+                               )
+                               
+                               
+                               ,
+                               
+                               circle = TRUE, status = "danger",
+                               icon = icon("gear"),size = "xs",
+                               
+                               tooltip = tooltipOptions(title = "Click to see inputs !")
+                             ))
+                    
+                    )
+                
+                
+              )),
+      # Tab Item: Time Series Analysis ----
+      tabItem(tabName = "tsAnalysis",
+              fluidRow(
+      
+                  
+               DTOutput("budget_final")
+               
+                
               )
               
               
       ),
+      
+      # Tab Item: Spending Patterns Analysis ----
+      tabItem(tabName = "spendingPatterns",
+              fluidRow(
+      
+                box(width = 12,
+                    title = "Number of transactions made by Month",
+                    id = "number_of_transactions_monthly",
+                    
+                    dropdownButton(
+                      
+                      tags$h3("Select year:"),
+                      
+                      awesomeRadio(
+                        inputId = "in_number_of_transactions",
+                        label = "Radio buttons", 
+                        choices = sort(unique(fin_data$yyyy)),
+                        selected = rev(sort(unique(fin_data$yyyy)))[1]
+                      )
+                      
+                      
+                      
+                      
+                      ,
+                      
+                      circle = TRUE, status = "danger",
+                      icon = icon("gear"),size = "xs",
+                      
+                      tooltip = tooltipOptions(title = "Click to see inputs !")
+                    ),
+                    tabPanel("Table", DTOutput("out_number_of_transactions")
+                    )
+                )    
+
+                
+              )
+              
+              
+      ),
+      
+      
+      
+      # Tab Item: Budget Making ----
+      tabItem(tabName = "budgetMaking",
+              fluidRow(
+                
+                box(width = 12,
+                    title = "Historical Averages", id = "historicalAverages",
+                    tabPanel(title = "Data", DTOutput(outputId = "out_historical_averages"))
+                    ),
+                
+                tabBox(width = 12,
+                    title = "Make budget", id = "budget_section",
+                    tabPanel(title = "Fill in values", 
+                             dataEditUI("budget_edit")),
+                    tabPanel("Finalised budget", id = "fin_budget",
+                             DTOutput("out_view_budget_final"),
+                             dataOutputUI("save_budget")))
+          
+                
+                
+                
+                
+                
+              )
+              
+              
+      ),
+      # Tab Item: Full Data Table ----
       tabItem(tabName = "fullDataTable",
               
               fluidRow(
